973311c5416078e5b6837dc304a967d6ab857440
Gustavo Perdomo <gperdomor@gmail.com>
2016-10-01 21:59:23 -0300

    Adjustes hechos por pato

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -24,8 +24,8 @@ export const fetchRequest = (options, callback) => {
         .then(function (response) {
             return callback(null, response, response);
         }).catch((error) => {
-        callback(error);
-    });
+            callback(error);
+        });
 };
 
 // const nodeRequest = function(options, callback) => {
@@ -91,44 +91,44 @@ const isLoading = (status) => {
 };
 
 const objectToQueryString = function (a) {
-        var prefix, s, add, name, r20, output;
-        s = [];
-        r20 = /%20/g;
-        add = function (key, value) {
-            // If value is a function, invoke it and return its value
-            value = ( typeof value == 'function' ) ? value() : ( value == null ? "" : value );
-            s[ s.length ] = encodeURIComponent(key) + "=" + encodeURIComponent(value);
-        };
-        if (a instanceof Array) {
-            for (name in a) {
-                add(name, a[name]);
-            }
-        } else {
-            for (prefix in a) {
-                buildParams(prefix, a[ prefix ], add);
-            }
-        }
-        output = s.join("&").replace(r20, "+");
-        return output;
+    var prefix, s, add, name, r20, output;
+    s = [];
+    r20 = /%20/g;
+    add = function (key, value) {
+        // If value is a function, invoke it and return its value
+        value = ( typeof value == 'function' ) ? value() : ( value == null ? "" : value );
+        s[s.length] = encodeURIComponent(key) + "=" + encodeURIComponent(value);
     };
-    function buildParams(prefix, obj, add) {
-        var name, i, l, rbracket;
-        rbracket = /\[\]$/;
-        if (obj instanceof Array) {
-            for (i = 0, l = obj.length; i < l; i++) {
-                if (rbracket.test(prefix)) {
-                    add(prefix, obj[i]);
-                } else {
-                    buildParams(prefix + "[" + ( typeof obj[i] === "object" ? i : "" ) + "]", obj[i], add);
-                }
-            }
-        } else if (typeof obj == "object") {
-            // Serialize object item.
-            for (name in obj) {
-                buildParams(prefix + "[" + name + "]", obj[ name ], add);
-            }
-        } else {
-            // Serialize scalar item.
-            add(prefix, obj);
+    if (a instanceof Array) {
+        for (name in a) {
+            add(name, a[name]);
+        }
+    } else {
+        for (prefix in a) {
+            buildParams(prefix, a[prefix], add);
         }
     }
+    output = s.join("&").replace(r20, "+");
+    return output;
+};
+function buildParams(prefix, obj, add) {
+    var name, i, l, rbracket;
+    rbracket = /\[\]$/;
+    if (obj instanceof Array) {
+        for (i = 0, l = obj.length; i < l; i++) {
+            if (rbracket.test(prefix)) {
+                add(prefix, obj[i]);
+            } else {
+                buildParams(prefix + "[" + ( typeof obj[i] === "object" ? i : "" ) + "]", obj[i], add);
+            }
+        }
+    } else if (typeof obj == "object") {
+        // Serialize object item.
+        for (name in obj) {
+            buildParams(prefix + "[" + name + "]", obj[name], add);
+        }
+    } else {
+        // Serialize scalar item.
+        add(prefix, obj);
+    }
+};
